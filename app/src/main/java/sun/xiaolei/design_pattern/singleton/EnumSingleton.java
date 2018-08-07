ba166bd7ac1765ee5e3197929b7d3fe6ad5cd923
sunxiaolei <xiaoleisun92@gmail.com>
2018-08-07 10:48:36 +0800

    create

---
+++ app/src/main/java/sun/xiaolei/design_pattern/singleton/EnumSingleton.java
@@ -0,0 +1,9 @@
+package sun.xiaolei.design_pattern.singleton;
+
+/*
+ * @author sun
+ * description:枚举单例
+ */
+public enum EnumSingleton {
+    INSTANCE
+}
---
+++ app/src/main/java/sun/xiaolei/design_pattern/singleton/SingletonManager.java
@@ -0,0 +1,29 @@
+package sun.xiaolei.design_pattern.singleton;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+ * @author sun
+ * description:容器管理
+ */
+public class SingletonManager {
+
+    private static Map<String, Object> instances = new HashMap<>();
+
+    private SingletonManager() {
+
+    }
+
+    public static void registerService(String key, Object instance) {
+        if (!instances.containsKey(key)) {
+            instances.put(key, instance);
+        }
+    }
+
+    public static Object getService(String key) {
+        return instances.get(key);
+    }
+
+
+}
